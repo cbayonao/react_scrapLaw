a23e1097ae3a9ad162f6223c030e942667ee6559
cbayonao <drcamilob@live.com>
2020-05-29 17:02:10 -0500

    [Fix] Add impor Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React from 'react';
-/*import logo from './logo.svg';*/
+import React, { Component } from 'react';
 import './App.css';
 import Login from './Login';
 import Home from './Home';

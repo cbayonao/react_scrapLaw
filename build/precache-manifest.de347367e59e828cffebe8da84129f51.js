74aec11c75e592b68a6050c203c7ebda416b37b0
cbayonao <drcamilob@live.com>
2020-05-29 11:57:20 -0500

    [Configuration files]

---
+++ build/precache-manifest.de347367e59e828cffebe8da84129f51.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c9b82962c978706a31f2f8ded80c553",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b391064de81c607ebe64",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "49b023f91b4a15e949dd",
+    "url": "/static/js/2.50a8bb38.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.50a8bb38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b391064de81c607ebe64",
+    "url": "/static/js/main.a5fc5a56.chunk.js"
+  },
+  {
+    "revision": "8a7d184be2a166d1ddca",
+    "url": "/static/js/runtime-main.fc6b72c5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
